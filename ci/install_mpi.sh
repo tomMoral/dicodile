85063005b5e99352ca0cd6fa1df02668dca98b0c
Romain Primet <romain.primet@inria.fr>
2021-01-12 20:39:40 +0100

    CI run pytest in github action (#1)
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

---
+++ ci/install_mpi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+case "$MPI_INSTALL" in
+    "conda")
+        conda install -y openmpi mpi4py;;
+    "system")
+        sudo apt-get install -qy libopenmpi-dev openmpi-bin;;
+    *)
+        false;;
+esac
