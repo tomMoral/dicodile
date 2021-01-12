1cf477e0df9cc62eb7baabd81f627ffbe43b7000
Romain PRIMET <romain.primet@inria.fr>
2021-01-12 18:07:41 +0100

    install openmpi-mpicc

--- ci/install_mpi.sh
+++ ci/install_mpi.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 case "$MPI_INSTALL" in
     "conda")
-        conda install -y openmpi;;
+        conda install -y openmpi openmpi-mpicc;;
     "system")
         sudo apt-get install -qy libopenmpi-dev openmpi-bin;;
     *)
