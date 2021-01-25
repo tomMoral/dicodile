82a1aca2d58431efe40a7ad9cd05ffedb4db3344
Romain Primet <romain.primet@inria.fr>
2021-01-25 18:34:30 +0100

    rename env variable and add tests

--- dicodile/data/home.py
+++ dicodile/data/home.py
@@ -2,14 +2,14 @@ import os
 from pathlib import Path
 
 # DATA_HOME is determined using environment variables.
-# The top priority is the environment variable $DICODILE_HOME which is
+# The top priority is the environment variable $DICODILE_DATA_HOME which is
 # specific to this package.
 # Else, it falls back on XDG_DATA_HOME if it is set.
 # Finally, it defaults to $HOME/data.
 # The data will be put in a subfolder 'dicodile'
 def get_data_home():
     data_home = os.environ.get(
-        'DICODILE_HOME', os.environ.get('XDG_DATA_HOME', None)
+        'DICODILE_DATA_HOME', os.environ.get('XDG_DATA_HOME', None)
     )
     if data_home is None:
         data_home = Path.home() / 'data'
---
+++ dicodile/data/tests/test_home.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from dicodile.data.home import get_data_home
+
+def test_dicodile_home(monkeypatch):
+    _set_env(monkeypatch, {
+        "DICODILE_DATA_HOME": "/home/unittest/dicodile"
+    })
+    assert(get_data_home() == Path("/home/unittest/dicodile/dicodile"))
+
+def test_XDG_DATA_home(monkeypatch):
+    _set_env(monkeypatch, {
+        "DICODILE_DATA_HOME":  None,
+        "XDG_DATA_HOME": "/home/unittest/data"
+    })  
+    assert(get_data_home() == Path("/home/unittest/data/dicodile"))
+
+def test_default_home(monkeypatch):
+    _set_env(monkeypatch, {
+        "HOME": "/home/default",
+        "DICODILE_DATA_HOME":  None,
+        "XDG_DATA_HOME": None,
+    })  
+    assert(get_data_home() == Path("/home/default/data/dicodile"))
+
+def test_dicodile_home_has_priority_over_xdg_data_home(monkeypatch):
+    _set_env(monkeypatch, {
+        "DICODILE_DATA_HOME":  None,
+        "XDG_DATA_HOME": None
+    })  
+    assert(get_data_home() == Path.home() / "data" / "dicodile")
+
+def _set_env(monkeypatch, d):
+    for k,v in d.items():
+        if v is not None:
+            monkeypatch.setenv(k,v)
+        else:
+            monkeypatch.delenv(k, raising=False)
