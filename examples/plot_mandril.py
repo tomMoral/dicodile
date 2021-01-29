993ab88d72284fd08eb29a8a2081a8ca36832787
Hande Gözükan <hande.gozukan@inria.fr>
2021-01-29 16:42:05 +0100

    init styles also when n_dict==1

--- examples/plot_mandril.py
+++ examples/plot_mandril.py
@@ -11,11 +11,15 @@ This example illlustrates reconstruction of `Mandrill image <http://sipi.usc.edu
 
 
 import numpy as np
-from dicodile.utils.dictionary import init_dictionary
 import matplotlib.pyplot as plt
+
+from dicodile.utils.dictionary import init_dictionary
 from dicodile.data.images import get_mandril
 
 from dicodile.utils.viz import display_dictionaries
+from dicodile.utils.dictionary import get_lambda_max
+
+from dicodile.update_z.dicod import dicod
 
 X = get_mandril()
 
@@ -37,6 +41,7 @@ atom_support = (8, 8)
 #
 rng = np.random.RandomState(60)
 
+
 D_init = init_dictionary(X, n_atoms, atom_support, random_state=rng)
 
 ###############################################################################
@@ -44,3 +49,25 @@ D_init = init_dictionary(X, n_atoms, atom_support, random_state=rng)
 
 list_D = np.array([D_init])
 display_dictionaries(*list_D)
+
+###############################################################################
+# Set parameters
+#
+reg = .01
+
+#
+tol = 5e-2
+
+#
+w_world = 7
+n_workers = w_world * w_world
+
+lmbd_max = get_lambda_max(X, D_init).max()
+reg_ = reg * lmbd_max
+
+###############################################################################
+# Run dicod
+
+# z_hat, *_ = dicod(X, D_init, reg_, max_iter=1000000, n_workers=n_workers,
+#                   tol=tol, strategy='greedy', verbose=1, soft_lock='none',
+#                   z_positive=False, timing=False)
