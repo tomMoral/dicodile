87ec4c80d6a3668ac1ef01c653fd7e26b363d210
Romain PRIMET <romain.primet@inria.fr>
2021-04-02 11:36:28 +0200

    Add package metadata.
    Co-authored-by: Hande Gözükan <hande.gozukan@inria.fr>

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+descr = """Distributed Convolutional Dictionary Learning"""
+
+DISTNAME = 'dicodile'
+DESCRIPTION = descr
+MAINTAINER = 'Thomas Moreau'
+MAINTAINER_EMAIL = 'thomas.moreau@inria.fr'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/tomMoral/dicodile.git'
 
 packages = ['dicodile',
             'dicodile.workers',
@@ -8,8 +16,25 @@ packages = ['dicodile',
             'dicodile.update_z',
             'dicodile.data']
 
-setup(name='dicodile',
-      version='0.1.dev0',
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      download_url=DOWNLOAD_URL,
+      long_description=open('README.rst').read(),
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved',
+          'Programming Language :: Python',
+          'Topic :: Software Development',
+          'Topic :: Scientific/Engineering',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+      ],
+      platforms='any',
+      version='0.1.dev1',
       packages=packages,
       install_requires=[
           'numpy',
