19b411b9101b104b92843bd5a37e380b5b7833ff
Hande Gözükan <hande.gozukan@inria.fr>
2021-05-07 16:00:12 +0200

    stop workers after tests are run

--- dicodile/update_z/tests/test_distributed_sparse_encoder.py
+++ dicodile/update_z/tests/test_distributed_sparse_encoder.py
@@ -51,3 +51,5 @@ def test_distributed_sparse_encoder():
     ztX = compute_ztX(z_hat, X)
     assert np.allclose(ztz, ztz_distrib)
     assert np.allclose(ztX, ztX_distrib)
+
+    encoder.shut_down_workers()
