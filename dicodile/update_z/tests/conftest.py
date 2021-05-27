8833ee46c3cb0187fbf2002ffd1df17435d7ba5e
Hande Gözükan <hande.gozukan@inria.fr>
2021-05-27 11:42:31 +0200

    Revert "run tests with mpirun command with host file provided as param"
    This reverts commit 79bc0f3675b0f54cd56a3e8e023b9db2b78d45a7.

--- dicodile/update_z/tests/conftest.py
+++ dicodile/update_z/tests/conftest.py
@@ -4,3 +4,4 @@ import os
 def pytest_configure(config):
     # Set DICOD in debug mode
     os.environ["TESTING_DICOD"] = "1"
+    os.environ["MPI_HOSTFILE"] = "dicodile/update_z/tests/hostfile_test"
