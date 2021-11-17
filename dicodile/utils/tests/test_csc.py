1057ca8a81076cb0f1fcb2578b5ad11499707d26
Romain PRIMET <romain.primet@inria.fr>
2021-11-17 10:45:48 +0100

    test convolve_multi_uv_shape for signal dimensions 1 to 3

--- dicodile/utils/tests/test_csc.py
+++ dicodile/utils/tests/test_csc.py
@@ -34,11 +34,13 @@ def test_ztz(valid_support, atom_support, sparsity):
     assert np.isclose(cost, np.dot(X_hat.ravel(), X_hat.ravel()))
 
 
-def test_dense_convolve_multi_uv_shape():
+@pytest.mark.parametrize('atom_single_channel_shape',
+                         [(35, ), (40, 30), (10, 12, 3)])
+def test_dense_convolve_multi_uv_shape(atom_single_channel_shape):
 
     n_channels = 3
-    sig_shape = (n_channels, 800, 600)
-    atom_shape = (n_channels, 40, 30)
+    sig_shape = (n_channels, *[8 * n for n in atom_single_channel_shape])
+    atom_shape = (n_channels, *atom_single_channel_shape)
     atom_support = atom_shape[1:]
     n_atoms = 25
     valid_support = get_valid_support(sig_support=sig_shape[1:],
