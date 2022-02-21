daf1913879940d3c1cb318c648338c7327a315c8
Romain PRIMET <romain.primet@inria.fr>
2022-02-21 13:32:06 +0100

    test

--- dicodile/tests/test_dicodile.py
+++ dicodile/tests/test_dicodile.py
@@ -24,14 +24,21 @@ def test_dicodile_greedy(n_workers):
     n_channels = 3
     n_atoms = 2
     n_times_atom = 10
+    n_times = 100
 
-    X, D, _ = simulate_data(n_times=100, n_times_atom=n_times_atom,
+    X, D, _ = simulate_data(n_times=n_times, n_times_atom=n_times_atom,
                             n_atoms=n_atoms, n_channels=n_channels,
                             noise_level=1e-5, random_state=42)
+    
+    #X = np.zeros((n_channels, n_times))
+    X *= 1e-6  # XXX
+    X[:,45:51] = np.ones((n_channels, 6)) * np.array([1, 0.5, 0.25]).reshape(3,1)
+    print(X)  # XXX
 
     # Starts with a single random atom, expect to learn others
     # from the largest reconstruction error patch
-    D[1:] = np.zeros((n_atoms-1, n_channels, n_times_atom))
+    # D[1:] = np.zeros((n_atoms-1, n_channels, n_times_atom))
+    D[1:] *= 1e-6
 
     D_hat, z_hat, pobj, times = dicodile(
         X, D, reg=.1, z_positive=True, n_iter=10, eps=1e-4,
