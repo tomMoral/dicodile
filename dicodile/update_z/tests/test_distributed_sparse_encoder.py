f647f802bc5537c87871b55b65435505e51b66d5
Romain Primet <romain.primet@inria.fr>
2022-09-27 10:54:03 +0200

    add test

--- dicodile/update_z/tests/test_distributed_sparse_encoder.py
+++ dicodile/update_z/tests/test_distributed_sparse_encoder.py
@@ -128,3 +128,56 @@ def test_compute_max_error_patch(n_workers):
     assert np.allclose(max_error_patch, reference_patch)
 
     encoder.shutdown_workers()
+
+
+def test_cannot_shrink_n_atoms():
+    pass
+
+@pytest.mark.parametrize('rank1', [True, False])
+def test_grow_n_atoms(rank1):
+    rng = check_random_state(42)
+
+    n_channels = 3
+    atom_support = (10,)
+    n_times = 10 * atom_support[0]
+    reg = 5e-1
+
+    params = dict(tol=1e-2, n_seg='auto', timing=False, timeout=None,
+                  verbose=100, strategy='greedy', max_iter=100000,
+                  soft_lock='border', z_positive=True, return_ztz=False,
+                  freeze_support=False, warm_start=False, random_state=27)
+
+    X = rng.randn(n_channels, n_times)
+
+    def make_dict(n_atoms):
+        if not rank1:
+            D = rng.randn(n_atoms, n_channels, *atom_support)
+            sum_axis = tuple(range(1, D.ndim))
+            D /= np.sqrt(np.sum(D * D, axis=sum_axis, keepdims=True))
+        else:
+            u = _prox_d(rng.randn(n_atoms, n_channels))
+            v = _prox_d(rng.randn(n_atoms, *atom_support))
+
+            D = u, v
+        return D
+
+    # Init dict with one atom
+    D = make_dict(1)
+
+    encoder = DistributedSparseEncoder(n_workers=3)
+    encoder.init_workers(X, D, reg, params, DtD=None)
+
+    # Process z hat
+    encoder.process_z_hat()
+
+    # Add one atom
+    # not quite what we would do in practice (we would add atoms and not make a new dict)
+    D = make_dict(2)
+    encoder.set_worker_D(D)
+
+    ## Process z hat
+    encoder.process_z_hat()
+    z_hat = encoder.get_z_hat()
+
+    ## Check z_hat shape
+    assert z_hat.shape[0] == 2
