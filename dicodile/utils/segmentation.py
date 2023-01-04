e846f70b634719a7f04a3a148144e15ca68d1640
Hande Gözükan <hande.gozukan@inria.fr>
2023-01-04 17:01:48 +0100

    move common methods to Segmentation

--- dicodile/utils/segmentation.py
+++ dicodile/utils/segmentation.py
@@ -43,6 +43,129 @@ class Segmentation:
         """Return the next segment indice in a cyclic way."""
         return (i_seg + 1) % self.effective_n_seg
 
+    def is_active_segment(self, i_seg):
+        """Return True if segment i_seg is active"""
+        return self._active_segments[i_seg]
+
+    def set_active_segments(self, indices):
+        """Activate segments indices and return the number of changed status.
+        """
+        if isinstance(indices, int):
+            indices = [indices]
+
+        n_changed_status = 0
+        for i_seg in indices:
+            n_changed_status += not self._active_segments[i_seg]
+            self._active_segments[i_seg] = True
+
+        self._n_active_segments += n_changed_status
+        assert self._n_active_segments <= self.effective_n_seg
+
+        return n_changed_status
+
+    def set_inactive_segments(self, indices):
+        """Deactivate segments indices and return the number of changed status.
+        """
+        if not np.iterable(indices):
+            indices = [indices]
+
+        n_changed_status = 0
+        for i_seg in indices:
+            n_changed_status += self._active_segments[i_seg]
+            self._active_segments[i_seg] = False
+
+        self._n_active_segments -= n_changed_status
+        return self._n_active_segments >= 0
+
+        return n_changed_status
+
+    def get_touched_segments(self, pt, radius, seg_pt=None,
+                             seg_bounds=None):
+        """Return all segments touched by an update in pt with a given radius.
+
+        Parameter
+        ---------
+        pt : list of int
+            Coordinate of the given update.
+        radius: int or list of int
+            Radius of the update. If an integer is given, use the same integer
+            for all axis.
+
+        Return
+        ------
+        segments : list of int
+            Indices of all segments touched by this update, including the one
+            in which the update took place.
+        """
+        assert len(pt) == self.n_axis
+        if isinstance(radius, int):
+            radius = [radius] * self.n_axis
+
+        for r, size_axis in zip(radius, self.seg_support):
+            if r >= size_axis:
+                raise ValueError("Interference radius is too large compared "
+                                 "to the segmentation size.")
+
+        i_seg = seg_pt
+        if i_seg is None:
+            i_seg = self.find_segment(pt)
+
+        if seg_bounds is None:
+            seg_bounds = self.get_seg_bounds(i_seg)
+
+        segments = [i_seg]
+        axis_offset = self.effective_n_seg
+        for x, r, n_seg_axis, (axis_start, axis_end), overlap_ax in zip(
+                pt, radius, self.n_seg_per_axis, seg_bounds, self.overlap):
+            axis_offset //= n_seg_axis
+            axis_i_seg = i_seg // axis_offset
+            i_seg %= axis_offset
+            new_segments = []
+            if x - r < axis_start + overlap_ax and axis_i_seg > 0:
+                new_segments.extend([n - axis_offset for n in segments])
+            if (x + r >= axis_start - overlap_ax or
+                    x - r < axis_end + overlap_ax):
+                new_segments.extend([n for n in segments])
+            if x + r >= axis_end - overlap_ax and axis_i_seg < n_seg_axis - 1:
+                new_segments.extend([n + axis_offset for n in segments])
+            segments = new_segments
+
+        for ii_seg in segments:
+            msg = ("Segment indice out of bound. Got {} for effective n_seg {}"
+                   .format(ii_seg, self.effective_n_seg))
+            assert ii_seg < self.effective_n_seg, msg
+
+        return segments
+
+    def find_segment(self, pt):
+        """Find the indice of the segment containing the given point.
+
+        If the point is not contained in the segmentation boundaries, return
+        the indice of the closest segment in manhattan distance.
+
+        Parameter
+        ---------
+        pt : list of int
+            Coordinate of the given update.
+
+        Return
+        ------
+        i_seg : int
+            Indices of the segment containing pt or the closest one in
+            manhattan distance if pt is out of range.
+        """
+        assert len(pt) == self.n_axis
+        i_seg = 0
+        axis_offset = self.effective_n_seg
+        for x, n_seg_axis, size_seg_axis, (axis_start, axis_end) in zip(
+                pt, self.n_seg_per_axis, self.seg_support, self.inner_bounds):
+            axis_offset //= n_seg_axis
+            axis_i_seg = max(min((x - axis_start) // size_seg_axis,
+                                 n_seg_axis - 1), 0)
+            i_seg += axis_i_seg * axis_offset
+
+        return i_seg
+
 
 class WorkerSegmentation(Segmentation):
     """Segmentation of a multi-dimensional signal and utilities to navigate it.
@@ -119,59 +242,6 @@ class WorkerSegmentation(Segmentation):
     def get_seg_support(self, seg_bounds):
         """Return a segment's shape"""
         return tuple(np.diff(seg_bounds, axis=1).squeeze(axis=1))
-
-    def get_touched_segments(self, pt, radius, i_seg,
-                             seg_inner_bounds):
-        """Return all segments touched by an update in pt with a given radius.
-
-        Parameter
-        ---------
-        pt : list of int
-            Coordinate of the given update.
-        radius: int or list of int
-            Radius of the update. If an integer is given, use the same integer
-            for all axis.
-
-        Return
-        ------
-        segments : list of int
-            Indices of all segments touched by this update, including the one
-            in which the update took place.
-        """
-        assert len(pt) == self.n_axis
-        if isinstance(radius, int):
-            radius = [radius] * self.n_axis
-
-        for r, size_axis in zip(radius, self.seg_support):
-            if r >= size_axis:
-                raise ValueError("Interference radius is too large compared "
-                                 "to the segmentation size.")
-
-        seg_bounds = seg_inner_bounds
-        segments = [i_seg]
-
-        axis_offset = self.effective_n_seg
-        for x, r, n_seg_axis, (axis_start, axis_end), overlap_ax in zip(
-                pt, radius, self.n_seg_per_axis, seg_bounds, self.overlap):
-            axis_offset //= n_seg_axis
-            axis_i_seg = i_seg // axis_offset
-            i_seg %= axis_offset
-            new_segments = []
-            if x - r < axis_start + overlap_ax and axis_i_seg > 0:
-                new_segments.extend([n - axis_offset for n in segments])
-            if (x + r >= axis_start - overlap_ax or
-                    x - r < axis_end + overlap_ax):
-                new_segments.extend([n for n in segments])
-            if x + r >= axis_end - overlap_ax and axis_i_seg < n_seg_axis - 1:
-                new_segments.extend([n + axis_offset for n in segments])
-            segments = new_segments
-
-        for ii_seg in segments:
-            msg = ("Segment indice out of bound. Got {} for effective n_seg {}"
-                   .format(ii_seg, self.effective_n_seg))
-            assert ii_seg < self.effective_n_seg, msg
-
-        return segments
 
     def get_local_coordinate(self, pt, seg_bounds):
         """Convert a point from global coordinate to local coordinate
@@ -406,129 +476,6 @@ class LocalSegmentation(Segmentation):
             seg_bounds.append([ax_bound_start, ax_bound_end])
             i_seg %= ax_offset
         return seg_bounds
-
-    def find_segment(self, pt):
-        """Find the indice of the segment containing the given point.
-
-        If the point is not contained in the segmentation boundaries, return
-        the indice of the closest segment in manhattan distance.
-
-        Parameter
-        ---------
-        pt : list of int
-            Coordinate of the given update.
-
-        Return
-        ------
-        i_seg : int
-            Indices of the segment containing pt or the closest one in
-            manhattan distance if pt is out of range.
-        """
-        assert len(pt) == self.n_axis
-        i_seg = 0
-        axis_offset = self.effective_n_seg
-        for x, n_seg_axis, size_seg_axis, (axis_start, axis_end) in zip(
-                pt, self.n_seg_per_axis, self.seg_support, self.inner_bounds):
-            axis_offset //= n_seg_axis
-            axis_i_seg = max(min((x - axis_start) // size_seg_axis,
-                                 n_seg_axis - 1), 0)
-            i_seg += axis_i_seg * axis_offset
-
-        return i_seg
-
-    def get_touched_segments(self, pt, radius, seg_pt=None,
-                             seg_bounds=None):
-        """Return all segments touched by an update in pt with a given radius.
-
-        Parameter
-        ---------
-        pt : list of int
-            Coordinate of the given update.
-        radius: int or list of int
-            Radius of the update. If an integer is given, use the same integer
-            for all axis.
-
-        Return
-        ------
-        segments : list of int
-            Indices of all segments touched by this update, including the one
-            in which the update took place.
-        """
-        assert len(pt) == self.n_axis
-        if isinstance(radius, int):
-            radius = [radius] * self.n_axis
-
-        for r, size_axis in zip(radius, self.seg_support):
-            if r >= size_axis:
-                raise ValueError("Interference radius is too large compared "
-                                 "to the segmentation size.")
-
-        i_seg = seg_pt
-        if i_seg is None:
-            i_seg = self.find_segment(pt)
-
-        if seg_bounds is None:
-            seg_bounds = self.get_seg_bounds(i_seg)
-
-        segments = [i_seg]
-        axis_offset = self.effective_n_seg
-        for x, r, n_seg_axis, (axis_start, axis_end), overlap_ax in zip(
-                pt, radius, self.n_seg_per_axis, seg_bounds, self.overlap):
-            axis_offset //= n_seg_axis
-            axis_i_seg = i_seg // axis_offset
-            i_seg %= axis_offset
-            new_segments = []
-            if x - r < axis_start + overlap_ax and axis_i_seg > 0:
-                new_segments.extend([n - axis_offset for n in segments])
-            if (x + r >= axis_start - overlap_ax or
-                    x - r < axis_end + overlap_ax):
-                new_segments.extend([n for n in segments])
-            if x + r >= axis_end - overlap_ax and axis_i_seg < n_seg_axis - 1:
-                new_segments.extend([n + axis_offset for n in segments])
-            segments = new_segments
-
-        for ii_seg in segments:
-            msg = ("Segment indice out of bound. Got {} for effective n_seg {}"
-                   .format(ii_seg, self.effective_n_seg))
-            assert ii_seg < self.effective_n_seg, msg
-
-        return segments
-
-    def is_active_segment(self, i_seg):
-        """Return True if segment i_seg is active"""
-        return self._active_segments[i_seg]
-
-    def set_active_segments(self, indices):
-        """Activate segments indices and return the number of changed status.
-        """
-        if isinstance(indices, int):
-            indices = [indices]
-
-        n_changed_status = 0
-        for i_seg in indices:
-            n_changed_status += not self._active_segments[i_seg]
-            self._active_segments[i_seg] = True
-
-        self._n_active_segments += n_changed_status
-        assert self._n_active_segments <= self.effective_n_seg
-
-        return n_changed_status
-
-    def set_inactive_segments(self, indices):
-        """Deactivate segments indices and return the number of changed status.
-        """
-        if not np.iterable(indices):
-            indices = [indices]
-
-        n_changed_status = 0
-        for i_seg in indices:
-            n_changed_status += self._active_segments[i_seg]
-            self._active_segments[i_seg] = False
-
-        self._n_active_segments -= n_changed_status
-        return self._n_active_segments >= 0
-
-        return n_changed_status
 
     def exist_active_segment(self):
         """Return True if at least one segment is active."""
