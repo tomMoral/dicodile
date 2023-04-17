9e8d692b038960fe798aab804ec9dabe7e29f4d8
Cedric Allain <cedric.allain31@gmail.com>
2023-04-17 20:43:28 +0200

    ENH add gait participants tsv

--- dicodile/data/gait.py
+++ dicodile/data/gait.py
@@ -3,11 +3,14 @@ from zipfile import ZipFile
 
 import pandas as pd
 from download import download
+from tqdm import tqdm
 
 from dicodile.config import DATA_HOME
 
+GAIT_CODE_LIST_FNAME = "gait_code_list.json"
 
-def get_gait_data(subject=1, trial=1):
+
+def get_gait_data(subject=1, trial=1, only_meta=False, verbose=True):
     """
     Retrieve gait data from this `dataset`_.
 
@@ -19,6 +22,10 @@ def get_gait_data(subject=1, trial=1):
     trial: int, defaults to 1
         Trial number.
         Valid subject-trial pairs can be found in this `list`_.
+    only_meta: bool, default to False
+        If True, only returns the subject metadata
+    verbose : bool, default to True
+        Whether to print download status to the screen.
 
     Returns
     -------
@@ -40,13 +47,59 @@ def get_gait_data(subject=1, trial=1):
     gait_dir.mkdir(parents=True, exist_ok=True)
     gait_zip = download(
         "http://dev.ipol.im/~truong/GaitData.zip",
-        gait_dir / "GaitData.zip"
+        gait_dir / "GaitData.zip",
+        verbose=verbose
     )
 
     with ZipFile(gait_zip) as zf:
         with zf.open(f"GaitData/{subject}-{trial}.json") as meta_file, \
-             zf.open(f"GaitData/{subject}-{trial}.csv") as data_file:
+                zf.open(f"GaitData/{subject}-{trial}.csv") as data_file:
             meta = json.load(meta_file)
-            data = pd.read_csv(data_file, sep=',', header=0)
-            meta['data'] = data
+            if not only_meta:
+                data = pd.read_csv(data_file, sep=',', header=0)
+                meta['data'] = data
             return meta
+
+
+def get_gait_code_list():
+    """Returns the list of all available codes.
+
+    Returns
+    -------
+    list
+        List of codes.
+    """
+    with open(GAIT_CODE_LIST_FNAME, "r") as f:
+        code_list = json.load(f)
+    return code_list
+
+
+def get_participants():
+    """Get the information relatives to all individual subjects, such as age,
+    gender, number of available trials, etc.  
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame with the informations of each individual subjects
+    """
+    all_codes = get_gait_code_list()
+    n_subjects = int(all_codes[-1].split('-')[0])
+
+    subject_rows = []
+    for subject in tqdm(range(1, n_subjects+1)):
+
+        meta = get_gait_data(subject, trial=1, only_meta=True, verbose=False)
+
+        for key in ['Trial', 'Code', 'LeftFootActivity', 'RightFootActivity']:
+            del meta[key]
+
+        subject_trials = [code for code in all_codes
+                          if code.split('-')[0] == str(subject)]
+        meta.update(n_trials=len(subject_trials))
+        subject_rows.append(meta)
+
+    participants = pd.DataFrame(subject_rows)
+    participants.to_csv('gait_particants.tsv', sep='\t', index=False)
+
+    return participants
