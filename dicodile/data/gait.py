27d4cfd900cb34d251071bb1a22534f9ac45268a
Cedric Allain <cedric.allain31@gmail.com>
2023-04-18 11:22:47 +0200

    update to retrieve files from archive

--- dicodile/data/gait.py
+++ dicodile/data/gait.py
@@ -1,13 +1,16 @@
+# %%
 import json
 from zipfile import ZipFile
 
 import pandas as pd
 from download import download
 from tqdm import tqdm
+import re
 
 from dicodile.config import DATA_HOME
 
 GAIT_CODE_LIST_FNAME = "gait_code_list.json"
+GAIT_PARTICIPANTS_FNAME = "gait_participants.tsv"
 
 
 def get_gait_data(subject=1, trial=1, only_meta=False, verbose=True):
@@ -69,8 +72,34 @@ def get_gait_code_list():
     list
         List of codes.
     """
-    with open(GAIT_CODE_LIST_FNAME, "r") as f:
-        code_list = json.load(f)
+    try:
+        with open(GAIT_CODE_LIST_FNAME, "r") as f:
+            code_list = json.load(f)
+
+    except FileNotFoundError:
+        gait_dir = DATA_HOME / "gait"
+        gait_dir.mkdir(parents=True, exist_ok=True)
+        gait_zip = download(
+            "http://dev.ipol.im/~truong/GaitData.zip",
+            gait_dir / "GaitData.zip",
+        )
+
+        with ZipFile(gait_zip) as zf:
+            all_files = zf.namelist()
+
+        code_list = []
+        for file in all_files:
+            code_list.extend(re.findall(r"\d+-\d+", file))
+
+        code_list = list(set(code_list))  # remove duplicates
+        # sort by subject id then by trial number
+        code_list.sort(key=lambda x: (
+            int(x.split('-')[0]), int(x.split('-')[1])))
+
+        # save as JSON
+        with open(GAIT_CODE_LIST_FNAME, 'w') as f:
+            json.dump(code_list, f, indent=2)
+
     return code_list
 
 
@@ -83,27 +112,34 @@ def get_participants():
     pandas.DataFrame
         A DataFrame with the informations of each individual subjects
     """
-    all_codes = get_gait_code_list()
-    n_subjects = int(all_codes[-1].split('-')[0])
 
-    subject_rows = []
-    for subject in tqdm(range(1, n_subjects+1)):
+    try:
+        participants = pd.read_csv(GAIT_PARTICIPANTS_FNAME, sep='\t')
 
-        meta = get_gait_data(subject, trial=1, only_meta=True, verbose=False)
+    except FileNotFoundError:
+        all_codes = get_gait_code_list()
+        n_subjects = int(all_codes[-1].split('-')[0])
 
-        for key in ['Trial', 'Code', 'LeftFootActivity', 'RightFootActivity']:
-            del meta[key]
+        subject_rows = []
+        for subject in tqdm(range(1, n_subjects+1)):
 
-        subject_trials = [code for code in all_codes
-                          if code.split('-')[0] == str(subject)]
-        meta.update(n_trials=len(subject_trials))
-        subject_rows.append(meta)
+            meta = get_gait_data(
+                subject, trial=1, only_meta=True, verbose=False)
 
-    participants = pd.DataFrame(subject_rows)
-    participants.to_csv('gait_participants.tsv', sep='\t', index=False)
+            for key in ['Trial', 'Code', 'LeftFootActivity', 'RightFootActivity']:
+                del meta[key]
+
+            subject_trials = [code for code in all_codes
+                              if code.split('-')[0] == str(subject)]
+            meta.update(n_trials=len(subject_trials))
+            subject_rows.append(meta)
+
+        participants = pd.DataFrame(subject_rows)
+        participants.to_csv(GAIT_PARTICIPANTS_FNAME, sep='\t', index=False)
 
     return participants
 
 
 if __name__ == '__main__':
+    get_gait_code_list()
     get_participants()
